c663a7835e8f54c23255a302824030d74b9c7787
dimassabimanyy <dmsabmny@gmail.com>
2020-12-19 15:18:27 +0700

    change navbar style

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-// Navigation
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active"); //Toggle nav
-
-    // Animate links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards`;
-      }
-      // console.log(index / 5 + 0.2);
-    });
-    // Burger animation
-    burger.classList.toggle("toggle");
+hamburger.addEventListener("click", () => {
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
   });
-};
 
-navSlide();
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
+});
